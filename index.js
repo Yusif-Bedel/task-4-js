342ae77b3727db0e2f865a265c06a4f17203d46d
Yusif <yusifbedel01@gmail.com>
2024-04-05 22:03:44 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+                                     //!task4-1
+// let kredit =parseInt(prompt("nece manat isteyirsen"))
+// let ay=parseInt(prompt("nece ay odeyecek"))
+// let pulodeme=kredit/ay
+// function money(kredit,ay) {
+//     for (let i = 1; i <= ay; i++) {
+//         console.log(i+ "ay" +(pulodeme |0)+"pul")
+//     }
+    
+// }
+// money(kredit,ay)
+                                    //!task4-2
+// let array=  [1,3,4,2,2]
+// function yusif(array) {
+//     let eded=0
+//     for (let i = 0; i < array.length; i++) {
+//         for (let j = 1; j < array.length; j++) {
+//             if(array[j]==array[i]){
+//                 eded=array[i]
+//             }
+//         }
+//     }
+//     return eded
+// }
+// console.log(yusif(array))
+                                   //!task4-3
+// function toCelsius(fahrenheit) {
+//   return (5/9) * (fahrenheit-32);
+// }
+
+// let value = toCelsius(parseInt(prompt("derece yaz brat")));
+// console.log(value)
